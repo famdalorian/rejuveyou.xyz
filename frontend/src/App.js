7175e7f6062d780ad5d45af4b86f209a6a6c8c2a
Soul-Scribes <168422977+Soul-Scribes@users.noreply.github.com>
2024-09-26 16:17:18 +0100

    added images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,28 @@
-import React, { useState } from 'react';
-import Header from './components/Header';
-import Services from './components/Services';
-import About from './components/About';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import Hero from './components/Hero';
-import Testimonials from './components/Testimonials';
-import Booking from './components/Booking';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Services from "./components/Services";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Testimonials from "./components/Testimonials";
+import Booking from "./components/Booking";
+import "./App.css";
 
 function App() {
-  const [activeSection, setActiveSection] = useState(''); // State to track active section
+  const [activeSection, setActiveSection] = useState(""); // State to track active section
 
   const renderSection = () => {
     switch (activeSection) {
-      case 'services':
+      case "services":
         return <Services />;
-      case 'booking':
+      case "booking":
         return <Booking />;
-      case 'about':
+      case "about":
         return <About />;
-      case 'testimonials':
+      case "testimonials":
         return <Testimonials />;
-      case 'contact':
+      case "contact":
         return <Contact />;
       default:
         return null; // No section is active by default
@@ -31,25 +31,33 @@ function App() {
 
   return (
     <div className="App">
-      <Header />
-      
-      {/* Hero section */}
-      <Hero />
-
       {/* Navigation for sections */}
       <nav>
         <ul>
-          <li><button onClick={() => setActiveSection('services')}>Services</button></li>
-         
-          <li><button onClick={() => setActiveSection('about')}>About</button></li>
-          <li><button onClick={() => setActiveSection('testimonials')}>Testimonials</button></li>
-          <li><button onClick={() => setActiveSection('contact')}>Contact</button></li>
-        </ul>
-      </nav>
+          <li>
+            <button onClick={() => setActiveSection("services")}>
+              Services
+            </button>
+          </li>
 
+          <li>
+            <button onClick={() => setActiveSection("about")}>About</button>
+          </li>
+          <li>
+            <button onClick={() => setActiveSection("testimonials")}>
+              Testimonials
+            </button>
+          </li>
+          <li>
+            <button onClick={() => setActiveSection("contact")}>Contact</button>
+          </li>
+        </ul>
+      </nav>{" "}
+      <Header />
+      {/* Hero section */}
+      <Hero />
       {/* Conditionally rendered sections */}
       {renderSection()}
-
       <Footer />
     </div>
   );
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -7,13 +7,13 @@ function Header() {
         <img src="/images/rjylogo.png" alt="Rejuve You Logo" className="logo" />
         <h1>Rejuve You</h1>
       </div>
-      <nav>
+      {/* <nav>
         <ul>
           <li><a href="#services">Services</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#contact">Contact</a></li>
         </ul>
-      </nav>
+      </nav> */}
     </header>
   );
 }
