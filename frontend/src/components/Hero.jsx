d42a858ca97032ddf56114c54bb75540fd6b1b8c
Soul-Scribes <168422977+Soul-Scribes@users.noreply.github.com>
2024-10-02 13:19:33 +0100

    added services css changes

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
   return (
     <section className="hero">
       <div className="hero-content">
-        <h1>Welcome to Our Service</h1>
+        <h1>Welcome to Rejuve You</h1>
         <p>Your journey to a better you,  starts here.</p>
         <button className="cta-button">talk to us Now</button>
       </div>
