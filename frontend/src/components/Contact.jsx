e98c71eb17e4319d445327fa57e9962380ffed03
Soul-Scribes <168422977+Soul-Scribes@users.noreply.github.com>
2024-10-14 20:14:31 +0100

    added link fixes

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -10,19 +10,19 @@ function Contact() {
       {/* Contact Options with Icons */}
       <div className="contact-icons">
         <div className="contact-item">
-          <FaEnvelope size={24} />
+          <FaEnvelope size={42} />
           <p>Email us <a href="mailto:info@beautyshop.com">Here</a></p>
         </div>
         <div className="contact-item">
-          <FaPhone size={24} />
+          <FaPhone size={42} />
           <p>Call us <a href="tel:+44 7967 397 257">Here</a></p>
         </div>
         <div className="contact-item">
-          <FaInstagram size={24} />
+          <FaInstagram size={42} />
           <p>Follow us on <a href="https://www.instagram.com/rejuve_you?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank" rel="noopener noreferrer">Instagram</a></p>
         </div>
         <div className="contact-item">
-          <FaFacebook size={24} />
+          <FaFacebook size={42} />
           <p>Like us on <a href="https://www.facebook.com/p/Rejuve-You-Aesthetics-100067734095254/?_rdr" target="_blank" rel="noopener noreferrer">Facebook</a></p>
         </div>
       </div>
